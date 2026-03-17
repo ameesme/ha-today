d9c7b1714b5cfebb8a8cf76c1ce88da6c7dfb21b
Mees <git@amees.me>
2026-03-17 12:38:13 +0100

    fix: fixing issue in self.config_entry

--- custom_components/ha_today/config_flow.py
+++ custom_components/ha_today/config_flow.py
@@ -135,10 +135,6 @@ class HAtodayConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 class HAodayOptionsFlow(config_entries.OptionsFlow):
     """Handle options flow for HA Today."""
 
-    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
-        """Initialize options flow."""
-        self.config_entry = config_entry
-
     async def async_step_init(
         self, user_input: dict[str, Any] | None = None
     ) -> FlowResult:
